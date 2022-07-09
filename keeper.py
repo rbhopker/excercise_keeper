9a6e7429d5db1155f0a130c7c7ba96b023b0a967
rbhopker <65057484+rbhopker@users.noreply.github.com>
2022-07-09 18:08:07 -0400

    Update keeper.py

--- keeper.py
+++ keeper.py
@@ -80,8 +80,8 @@ def retrieve_from_server(date,user):
         return rows
     
     sheet_url = st.secrets["private_gsheets_url"]
-    # st.write(f'SELECT * FROM "{sheet_url}" WHERE user = "{user}"')
-    rows = run_query(f'SELECT * FROM "{sheet_url}"')
+    st.write(f'SELECT * FROM "{sheet_url}" WHERE user = "{user}"')
+    rows = run_query(f'SELECT * FROM "{sheet_url}" WHERE user = "{user}"')
     
     # Print results.
     for row in rows:
