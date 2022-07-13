dbbd1ab2ed6865c08ec3085dae0496d10e95ed2a
rbhopker <65057484+rbhopker@users.noreply.github.com>
2022-07-12 23:08:23 -0400

    Update keeper.py

--- keeper.py
+++ keeper.py
@@ -67,7 +67,7 @@ def check_password():
 def query_to_text(user,rows):
     if list(rows)==[]:
         txt = [f"{user} nao anotou nenhum exercicio nesta data"]
-    else:txt
+    else:
         txt = []
         for row in rows:
             txt.append(f"{row[1]}, {row[0]}, Exercicio: {row[2]}, Detalhes: {row[3]}, Comentarios: {row[4]}")
